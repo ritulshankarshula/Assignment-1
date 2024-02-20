c07c940f9921c7a50f598cc33966ced31fc145be
ritul shankar shukla <ritulshankarshuk@gmail.com>
2024-02-20 08:37:40 +0530

    title change

--- Ass 1.php
+++ Ass 1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Assignment 1 solution</title>
 </head>
 <body bgcolor="red">
 
